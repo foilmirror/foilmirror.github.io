70ec271b2264c4e81d03db3a0a0110312e01e71d
Spencer Jonas <spencerjonas@gmail.com>
2023-12-19 23:45:49 -0500

    Added hwish to useful moves

--- static/scripts/draftplanner.js
+++ static/scripts/draftplanner.js
@@ -101,7 +101,7 @@ function appendPokemon(item,gen){
     useful=[]
     //listing prio here is too much
     //usefulmoveslist=["Stealth Rock","Spikes","Toxic Spikes","Sticky Web","Defog","Rapid Spin","Court Change","Heal Bell","Aromatherapy","Wish","Fake Out","Extreme Speed","First Impression","Accelrock","Aqua Jet","Bullet Punch","Ice Shard","Mach Punch","Quick Attack","Shadow Sneak","Sucker Punch","Vacuum Wave","Water Shuriken","U-turn","Volt Switch","Parting Shot","Teleport","Flip Turn","Knock Off"]
-    usefulmoveslist=["Stealth Rock","Spikes","Toxic Spikes","Sticky Web","Defog","Rapid Spin","Court Change","Mortal Spin","Tidy Up","Heal Bell","Aromatherapy","Wish","Shed Tail","U-turn","Volt Switch","Parting Shot","Flip Turn","Chilly Reception","Knock Off"]
+    usefulmoveslist=["Stealth Rock","Spikes","Toxic Spikes","Sticky Web","Defog","Rapid Spin","Court Change","Mortal Spin","Tidy Up","Healing Wish","Lunar Dance","Heal Bell","Aromatherapy","Wish","Shed Tail","U-turn","Volt Switch","Parting Shot","Flip Turn","Chilly Reception","Knock Off"]
     if(gen == "gen8" || gen == "gen9") {
       usefulmoveslist.push("Teleport")
     }
